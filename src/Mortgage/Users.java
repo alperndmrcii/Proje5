023028a3c8faf49911fc06aebaf03ce008ff9e6f
Alperen Demirci <alperndmrcii@gmail.com>
2023-01-27 20:32:33 +0300

    proje başlangıcı

---
+++ src/Mortgage/Users.java
@@ -0,0 +1,50 @@
+package Mortgage;
+
+public class Users {
+
+    /*
+
+        User Class, HousePrices Class ının ebeveyn classidir.
+
+        her bir User in almasi gereken attribute lar private olmalıdır ve su sekildedir:
+
+        String olanlar -> username, whichCondtiton, houseType
+        int olanlar -> roomCount, downPayment, longTerm
+
+        ve son olarakta StatesTax cinsinden bir StatesTax olmalıdır:
+
+        "private StatexTax statesTax;"
+
+
+     */
+
+    //--------------------------------------------------------------
+    // -> Attributes
+    //--------------------------------------------------------------
+
+
+
+
+    /*
+
+        Bir constructor methodu oluşturup, tüm attribute lar constructorda gelmeli ve daha sonra sınıfın kendi attributelarına
+    eşitlenmelidir.
+
+     */
+
+    //--------------------------------------------------------------
+    // -> Constructor
+    //--------------------------------------------------------------
+
+
+
+
+
+
+    // Son olarak ise tüm attributelar icin sadece getter methodlar oluşmalıdır.
+
+    //--------------------------------------------------------------
+    // -> Methods
+    //--------------------------------------------------------------
+}
+
